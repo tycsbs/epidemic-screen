b228dcea09f5d28013cf181a592b40334c5dc739
baisong01 <baisong01@longfor.com>
2022-11-14 11:36:18 +0800

    增加charts组件封装

---
+++ src/components/core/echart.vue
@@ -0,0 +1,84 @@
+<template>
+    <div ref="chartRef" :id="id">
+        
+    </div>
+</template>
+<script setup lang='ts'>
+import { ref, reactive, onMounted, watch } from 'vue'
+import * as echarts from 'echarts'
+import type { EChartsType, EChartsOption } from 'echarts'
+import geoJson from '@/assets/china.json'
+
+const props = defineProps({
+    /**
+     * id: 容器ID, 唯一值
+     */
+    id: String,
+
+    /**
+     * 容器宽度
+     */
+    width: {
+        type: String,
+        required: true
+    },
+
+    /**
+     * 容器高度
+     */
+    height: {
+        type: String,
+        required: true
+    },
+
+    /**
+     * 自定义样式类名
+     */
+    className: String,
+
+    /**
+     * 图标参数
+     */
+    options: {
+        type: Object,
+        default: () => ({}),
+    },
+
+    /**
+     * 需要加载的地图区域名称
+     */
+    mapName: String
+
+})
+
+const chartRef = ref()
+const chartInstanceRef = ref<EChartsType>()
+
+onMounted(() => {
+    chartInstanceRef.value = echarts.init(chartRef.value, {})
+    props.mapName && echarts.registerMap(props.mapName, geoJson as any)
+    initChart()
+
+})
+
+watch(() => props.options, (opt) => {
+    opt && initChart(opt)
+}, {
+    deep: true
+})
+
+const initChart = (data?: EChartsOption) => {
+    const opt = data || props.options
+    opt && chartInstanceRef.value?.setOption(opt)
+}
+
+defineExpose({
+    initChart
+})
+</script>
+
+<style lang='less' scoped>
+.container {
+    
+}
+</style>
